efe441e76de1af286b919e03e0e8dbf70387e6fc
Karsten W <brox.p@web.de>
2021-08-11 06:53:15 +0200

    update

---
+++ travis-build/src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travis_build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
