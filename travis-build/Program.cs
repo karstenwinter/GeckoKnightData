3e6a8b999112195e7b90b91f477f0d26d2ef12ef
Karsten W <brox.p@web.de>
2021-08-11 08:06:33 +0200

    update

--- travis-build/Program.cs
+++ travis-build/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,37 @@ namespace travis_build
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            Console.ReadKey();
+            var sb = new StringBuilder("Dir\tSubDir\tFile\tLength\t(this file is generated)\n");
+            // list("../../../", "", Console.WriteLine);
+
+            var root = "../../../";
+            foreach (var dir in Directory.GetDirectories(root))
+            {
+                var dirI = new DirectoryInfo(dir);
+
+
+                foreach (var subDir in Directory.GetDirectories(dir))
+                {
+                    var subDirI = new DirectoryInfo(subDir);
+                    foreach (var file in Directory.GetFiles(subDir))
+                    {
+                        var f = new FileInfo(file);
+                        if (f.Name.Contains("png"))
+                            sb.AppendLine(dirI.Name + "\t" + subDirI.Name + "\t" + f.Name + "\t" + f.Length);
+                    }
+                }
+                foreach (var file2 in Directory.GetFiles(dir))
+                {
+                    var f2 = new FileInfo(file2);
+                    if (f2.Name.Contains("png"))
+                        sb.AppendLine(dirI.Name + "\t" + "-"  + "\t" + f2.Name + "\t" + f2.Length);
+                }
+            }
+
+            Console.WriteLine(sb);
+            File.WriteAllText("output.tsv", sb.ToString());
+            // TODO remove
+            //Console.ReadKey();
         }
     }
 }
